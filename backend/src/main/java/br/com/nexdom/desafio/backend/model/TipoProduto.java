2e8ff18269ed576b4d3fe8fea3ecc05bce938851
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 00:55:47 -0300

    Add TipoProduto entity to represent product types in the backend model

---
+++ backend/src/main/java/br/com/nexdom/desafio/backend/model/TipoProduto.java
@@ -0,0 +1,27 @@
+package br.com.nexdom.desafio.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entidade que representa um tipo de produto.
+ */
+@Entity
+@Table(name = "tipo_produto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoProduto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tipo_produto_seq")
+    @SequenceGenerator(name = "tipo_produto_seq", sequenceName = "tipo_produto_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "O nome do tipo de produto é obrigatório")
+    private String nome;
+}
