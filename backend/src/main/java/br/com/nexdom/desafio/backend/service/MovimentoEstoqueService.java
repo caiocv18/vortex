60ce82d897352b43aff61fe7d2405f7f8f22e82b
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 01:24:55 -0300

    Refactor MovimentoEstoqueService to replace Collectors.toList() with Stream.toList()

--- backend/src/main/java/br/com/nexdom/desafio/backend/service/MovimentoEstoqueService.java
+++ backend/src/main/java/br/com/nexdom/desafio/backend/service/MovimentoEstoqueService.java
@@ -98,7 +98,7 @@ public class MovimentoEstoqueService {
         
         return movimentos.stream()
                 .map(this::mapToDTO)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     /**
