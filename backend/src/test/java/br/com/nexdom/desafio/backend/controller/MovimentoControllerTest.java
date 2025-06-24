f77c6ae86a256172cf073daa5bf60ed0d8c6ade3
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 01:04:13 -0300

    Add MovimentoControllerTest to validate MovimentoController functionalities

---
+++ backend/src/test/java/br/com/nexdom/desafio/backend/controller/MovimentoControllerTest.java
@@ -0,0 +1,36 @@
+package br.com.nexdom.desafio.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MovimentoControllerTest {
+
+    @Autowired
+    private MovimentoController movimentoController;
+
+    @Test
+    public void testControllerLoads() {
+        // This test will pass if the controller is autowired successfully
+        assertNotNull(movimentoController);
+    }
+
+    @Test
+    public void testBuscarTodos() {
+        // This test will verify that we can get all inventory movements
+        ResponseEntity<?> response = movimentoController.buscarTodos();
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+}
