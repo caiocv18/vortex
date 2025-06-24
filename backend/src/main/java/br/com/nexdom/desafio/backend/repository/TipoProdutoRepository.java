db38cf4af3f9070e24a93acbc342b4d6cccfc4f0
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 00:57:58 -0300

    Add TipoProdutoRepository to manage TipoProduto data interactions

---
+++ backend/src/main/java/br/com/nexdom/desafio/backend/repository/TipoProdutoRepository.java
@@ -0,0 +1,30 @@
+package br.com.nexdom.desafio.backend.repository;
+
+import br.com.nexdom.desafio.backend.model.TipoProduto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository para a entidade TipoProduto.
+ */
+@Repository
+public interface TipoProdutoRepository extends JpaRepository<TipoProduto, Long> {
+    
+    /**
+     * Busca um tipo de produto pelo nome.
+     * 
+     * @param nome Nome do tipo de produto
+     * @return Optional contendo o tipo de produto, se encontrado
+     */
+    Optional<TipoProduto> findByNome(String nome);
+    
+    /**
+     * Verifica se existe um tipo de produto com o nome informado.
+     * 
+     * @param nome Nome do tipo de produto
+     * @return true se existir, false caso contrário
+     */
+    boolean existsByNome(String nome);
+}
