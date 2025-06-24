accb27c909f47017108e5ab3efea9173681acbba
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 01:21:45 -0300

    Refactor BasicApplicationTest to remove public modifiers from class and method

--- backend/src/test/java/br/com/nexdom/desafio/backend/BasicApplicationTest.java
+++ backend/src/test/java/br/com/nexdom/desafio/backend/BasicApplicationTest.java
@@ -6,10 +6,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class BasicApplicationTest {
+class BasicApplicationTest {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
         // This test will pass if the application context loads successfully
     }
 }
