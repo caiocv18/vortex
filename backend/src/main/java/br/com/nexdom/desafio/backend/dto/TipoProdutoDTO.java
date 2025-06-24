19454d4a9d3a7e58287a3e9edb0de0ba434ff087
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 00:59:41 -0300

    Add TipoProdutoDTO to transfer TipoProduto data in the backend model

---
+++ backend/src/main/java/br/com/nexdom/desafio/backend/dto/TipoProdutoDTO.java
@@ -0,0 +1,24 @@
+package br.com.nexdom.desafio.backend.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO para transferência de dados de TipoProduto.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO para transferência de dados de Tipo de Produto")
+public class TipoProdutoDTO {
+
+    @Schema(description = "ID do tipo de produto", example = "1")
+    private Long id;
+
+    @NotBlank(message = "O nome do tipo de produto é obrigatório")
+    @Schema(description = "Nome do tipo de produto", example = "Eletrônicos", required = true)
+    private String nome;
+}
