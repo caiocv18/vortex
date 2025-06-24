66c66aea91ad1a4b82b6f7543806db129335024d
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-24 00:56:05 -0300

    Add TipoMovimentacao enum to represent stock movement types in the backend model

---
+++ backend/src/main/java/br/com/nexdom/desafio/backend/model/enums/TipoMovimentacao.java
@@ -0,0 +1,9 @@
+package br.com.nexdom.desafio.backend.model.enums;
+
+/**
+ * Enum que representa os tipos de movimentação de estoque.
+ */
+public enum TipoMovimentacao {
+    ENTRADA,
+    SAIDA
+}
