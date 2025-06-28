c3fa8dd6fd9c953d4a4dfb54743c20997519829a
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-28 08:26:25 -0300

    Update CORS configuration to include additional allowed origins for local development

--- backend/src/main/java/br/com/nexdom/desafio/backend/config/CorsConfig.java
+++ backend/src/main/java/br/com/nexdom/desafio/backend/config/CorsConfig.java
@@ -17,12 +17,14 @@ public class CorsConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000",
+            "http://localhost:4173",
             "http://localhost:5173",
             "http://localhost:5174",
-            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://127.0.0.1:4173",
             "http://127.0.0.1:5173",
-            "http://127.0.0.1:5174",
-            "http://127.0.0.1:3000"
+            "http://127.0.0.1:5174"
         ));
         corsConfiguration.setAllowedHeaders(Arrays.asList(
             "Origin",
